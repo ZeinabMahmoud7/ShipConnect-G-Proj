494a8e4bc101347b284dfe57522ae3ea573665e5
Roaa Muhammed <88007570+RoaaBadr@users.noreply.github.com>
2025-07-13 13:36:16 +0300

    App routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,23 @@ import SettingShipping from './pages/Setting/SettingShipping';
 import OffersShipping from './pages/Offers/OffersShipping';
 import OffersShippingDetailes from './pages/Offers/OffersShippingDetailes';
 import ShipmentsListShipping from './pages/ShippmentList/ShipmentsListShipping';
+
 import DeliverShippingShipment from './pages/CompanyStatus/DeliverShippingShipment';
 import OnTransitShippingShipment from './pages/CompanyStatus/OnTransitShippingShipment';
+import PendingShippingShipment from './pages/CompanyStatus/PendingShippingShipment';
+
+import DeliverStartupShipment from './pages/StartupStatus/DeliverStartupShipment';
+import OnTransitStartupShipment from './pages/StartupStatus/OnTransitStartupShipment';
+import PendingStartupShipment from './pages/StartupStatus/PendingStartupShipment';
+
 import TrackShipment from './pages/TrackShipment';
-import PendingShippingShipment from './pages/PendingShippingShipment';
 import { AuthProvider } from './Context/AuthContext';
 import LoginForm from './Components/Auth/LoginForms';
 import StartupRegisterForm from './Components/Auth/StartupRegisterForm';
 import CompanyRegisterForm from './Components/Auth/CompanyRegisterForm';
 import ForgotPassword from './Components/Auth/ForgotPassword';
 import { Toaster } from 'react-hot-toast';
+import ProtectedRoute from "./Components/Auth/ProtectedRoute "
 
 function App() {
   const [shipments, setShipments] = useState(mockShipments);
@@ -91,14 +98,14 @@ function App() {
               <Route path="settingsShipping" element={<SettingShipping />} />
               <Route path="contactShipping" element={<ContactShipping />} />
 
-            {/* Dashboard LayoutAdmin routes */}
-<Route path="/dashboardAdmin" element={<LayoutAdmin />}>
-  <Route index element={<DashboardAdmin />} />
-  <Route path="SettingAdmin" element={<SettingAdmin />} />
-  <Route path="Partners" element={<Partners />} />
-  <Route path="shipping-details/:id" element={<ShippingProfile />} />
-  
-</Route>
+              {/* Dashboard LayoutAdmin routes */}
+              {/* <Route path="/dashboardAdmin" element={<LayoutAdmin />}>
+                <Route index element={<DashboardAdmin />} />
+                <Route path="SettingAdmin" element={<SettingAdmin />} />
+                <Route path="Partners" element={<Partners />} />
+                <Route path="shipping-details/:id" element={<ShippingProfile />} />
+
+              </Route> */}
 
               <Route path="shipmentsShipping" element={<ShipmentsListShipping shipments={shipmentsShipping} setShipments={setShipmentsShipping} />} />
               <Route path="shipmentsShipping/shipment/:id" element={<DeliverShippingShipment />} />
